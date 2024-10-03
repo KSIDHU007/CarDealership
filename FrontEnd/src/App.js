830d3ab4861a350a493c2d32c1db25eecb002d70
Tarun1312 <144748933+Tarun1312@users.noreply.github.com>
2024-10-03 17:45:08 -0600

    Add Front-end for book appointment page

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AppointmentPage from './components/AppointmentPage';
+import './components/style.css';
+
+
+function App() {
+  return (
+    <div id="Container">
+
+      <h1>Book Appointment</h1>
+      
+      <AppointmentPage />
+    </div>
+  );
+}
+
+export default App;
